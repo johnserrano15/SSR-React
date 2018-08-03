d535eed7ab795fce5ce72f536cc7ad0ce1c50c01
John Serrano <andrey_201015@hotmail.com>
2018-08-03 15:29:28 -0500

    Initial commit config like ESlint, standard y prettier

---
+++ webpack.common.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+module.exports = {
+  entry: {
+    home: path.resolve(__dirname, 'src/home.jsx')
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist/public'),
+    filename: 'js/[name].js',
+    publicPath: '/'
+  },
+  plugins: [new ExtractTextPlugin('css/[name].css')],
+  module: {
+    rules: [
+      {
+        // test: que tipo de archivo quiero reconocer,
+        // use: que loader se va a encargar del archivo
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es2015', 'react', 'stage-2']
+          }
+        }
+      },
+      {
+        test: /\.(sass|scss)$/,
+        exclude: /(node_modules)/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  }
+}
