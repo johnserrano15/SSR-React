d6fbb37760482267efca445c0e7c132cec8ce6cb
John Serrano <andrey_201015@hotmail.com>
2018-08-04 09:17:09 -0500

    Cambios en eslint y standard con jsx

--- src/home.jsx
+++ src/home.jsx
@@ -3,10 +3,16 @@ import React, { Component } from 'react'
 import { render } from 'react-dom'
 
 class Home extends Component {
+  constructor (props) {
+    super(props)
+    this.hola = 'hola mundo'
+  }
+
   render () {
     return (
       <section className='Home'>
         <h2>Hola mundo :)</h2>
+        <h4>{this.props.saludo}</h4>
       </section>
     )
   }
@@ -14,4 +20,4 @@ class Home extends Component {
 
 const App = document.getElementById('home')
 
-render(<Home />, App)
+render(<Home saludo='Hola mundo' />, App)
