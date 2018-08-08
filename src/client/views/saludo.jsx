45b8760268df08267f2ab803ffac3e66938334cf
John Serrano <andrey_201015@hotmail.com>
2018-08-08 15:36:17 -0500

    unistall algunos package y no funciona

--- src/client/views/saludo.jsx
+++ src/client/views/saludo.jsx
@@ -1,15 +1,7 @@
 import React from 'react'
 import Layout from './layouts/default.jsx'
 
-// const Saludo = props => {
-//   return (
-//     <Layout>
-//       <h4>Hola soy un mensaje</h4>
-//     </Layout>
-//   )
-// }
-
-function Saludo (props) {
+const Saludo = props => {
   console.log(props)
   return (
     <Layout>
